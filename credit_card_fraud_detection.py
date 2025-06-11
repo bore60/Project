1728ebcda9c224a0b9bedd29ddc4a0f509c2e3ba
bore60 <sylviabore261@gmail.com>
2025-06-11 21:48:16 +0300

    Update credit_card_fraud_detection.py

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -17,10 +17,7 @@ import time
 st.title("💳 Credit Card Fraud Detection")
 st.markdown("**👩‍💻 Created by Sylvia Chelangat Bore**")
 st.markdown("Upload the **creditcard.csv** file to begin analysis.")
-st.markdown(
-    '<small>📥 <a href="https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud" target="_blank">Download dataset from Kaggle</a></small>',
-    unsafe_allow_html=True
-)
+st.markdown('<small>📥 <a href="https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud" target="_blank">Download dataset from Kaggle</a></small>',unsafe_allow_html=True)
 
 # === File Uploader === #
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
